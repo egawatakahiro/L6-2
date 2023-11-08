a7671b53bec086a7153957c9ae2d58feab9683f5
Takahiro Egawa <2111140085a@ed.fuk.kindai.ac.jp>
2023-11-08 04:18:56 +0000

    微調整

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,17 +1,33 @@
 class CartItemsController < ApplicationController
   def new
+    #@products = Product.find[params[:product_id]]
   end
 
   def create
-    @cart_item = CartItem.new(
-      qty: params[:qty],
-      product_id: params[:id],
-      cart_id: 1)
-    @cart_item.save
+    # @cart_item = CartItem.new(
+    #   qty: params[:qty],
+    #   product_id: params[:id],
+    #   cart_id: 1)
+    # @cart_item.save
     
-    redirect_to root_path
+    # redirect_to root_path
+    @cart_item = CartItem.new(cart_item_params)
+    if @cart_item.save
+      redirect_to cart_psth(@cart_item.cart_id)
+    else
+      redirect_to products_path
+    end
   end
 
   def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to cart_path(@cart_item.cart_id), notice: 'Product was successfully remove from the cart'
+  end
+  
+  private
+  
+  def cart_item_params
+    params.require(:cart_item).permit(:qty, :product_id).marge(cart_id: 1)
   end
 end
